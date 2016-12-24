1eff1720bb72fae6c778e2d2953be6165e371000
mohancm100 <mohanmanjappa@gmail.com>
2016-12-24 18:04:27 +0530

    Add missing touchscreen drivers

---
+++ mohan.sh
@@ -0,0 +1,11 @@
+export KBUILD_BUILD_USER="Mohancm"
+export KBUILD_BUILD_HOST="A.M.T"
+export CROSS_COMPILE=/home/mohancm/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+export ARCH=arm64
+
+make clean && make mrproper
+
+make k5fpr_defconfig
+
+make -j8
