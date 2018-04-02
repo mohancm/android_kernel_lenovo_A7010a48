29cf103bcfa28dad307266a64f8cf71a86f6e979
Charles Keepax <ckeepax@opensource.wolfsonmicro.com>
2018-04-02 17:56:23 +0300

    switch: arizona: Remove enable/disable of 32k clock
    The 32k clock is unconditionally enabled by the MFD core so there is no
    need to control it from the Extcon device.
    
    Change-Id: Ib97338ec56be79c33051b5eb7b2e8fbf33267d95
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/switch/switch-arizona.c
+++ drivers/switch/switch-arizona.c
@@ -3448,8 +3448,6 @@ static int arizona_extcon_probe(struct platform_device *pdev)
 		goto err_micdet;
 	}
 
-	arizona_clk32k_enable(arizona);
-
 	switch (arizona->type) {
 	case WM5102:
 	case WM5110:
@@ -3570,7 +3568,6 @@ static int arizona_extcon_remove(struct platform_device *pdev)
 	cancel_delayed_work_sync(&info->hpdet_work);
 	regmap_update_bits(arizona->regmap, ARIZONA_JACK_DETECT_ANALOGUE,
 			   ARIZONA_JD1_ENA | ARIZONA_JD2_ENA, 0);
-	arizona_clk32k_disable(arizona);
 
 	device_remove_file(&pdev->dev, &dev_attr_hp_impedance);
 	device_remove_file(&pdev->dev, &dev_attr_mic_impedance);
